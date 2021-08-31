301e6c0ecb4cb7ac3362dc968ad3414dac1baaef
Daniel Gutierrez <danielgudu22@gmail.com>
2021-08-30 22:41:27 -0500

    partials and process of render colections

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,18 @@
 class ArticlesController < ApplicationController
+	before_action :find_article, only: [:show, :edit, :update, :destroy]
+							   # except
+		
+	def index
+		@articles = Article.all
+	end	
 
 	def show
-		@article = Article.find(params[:id])
 	end
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	def update
-		@article = Article.find(params[:id])
 		@article.update(title: params[:article][:title], content: params[:article][:content])
 		redirect_to @article
 	end
@@ -23,10 +26,13 @@ class ArticlesController < ApplicationController
 		render json: @article
 	end
 
-	def destroy
-		@article = Article.find(params[:id])
+	def destroys
 		@article.destroy
 		redirect_to root_path
+	end
+
+	def find_article
+		@article = Article.find(params[:id])
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
+  get "/articles", to: "articles#index"
   get "/articles/new", to: "articles#new"
   get "/articles/:id", to: "articles#show"
   get "/articles/:id/edit", to: "articles#edit"

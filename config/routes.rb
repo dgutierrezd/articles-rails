adedf83fb7d6e5a8d1b6348ba466f1c5caeb24b6
Daniel Gutierrez <danielgudu22@gmail.com>
2021-09-03 10:22:26 -0500

    Se creo layout en partials y se renderiza las colecciones

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get "/articles", to: "articles#index"
-  get "/articles/new", to: "articles#new"
+  get "/articles/new", to: "articles#new", as: :new_articles
   get "/articles/:id", to: "articles#show"
   get "/articles/:id/edit", to: "articles#edit"
 
